d05c4fe46a55af0726b83788261d91f0fcbd58f9
100rabhc_smc <saurabhchavan052@gmail.com>
2021-07-22 18:38:17 +0530

    Component

---
+++ Day5-relish-remainder/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Relishremainder from './component/Relishremainder';
+
+function App() {
+  return (
+    <div className="App">
+      <Relishremainder/>
+    </div>
+  );
+}
+
+export default App;
